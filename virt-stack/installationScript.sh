8c485187e752485b1b486be18bd8c8c1890f0462
polotiran <150043014+polotiran@users.noreply.github.com>
2025-10-25 10:45:45 +0200

    update post tests

--- virt-stack/installationScript.sh
+++ virt-stack/installationScript.sh
@@ -7,8 +7,7 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 echo "Don't forget this script installs QEMU/KVM and libvirt, it does not install eventual dependancies required !"
-echo "From what I have experienced everything else you need is pre-installed on red-hat like systems, I do not know about others"
-# Detect package manager
+echo "It has been tested on Debian13 and on Rocky10, but it should also work with other distributions"
 if command -v apt &> /dev/null; then
     PKG_MANAGER="apt"
 elif command -v dnf &> /dev/null; then
@@ -17,8 +16,6 @@ else
     echo "Neither apt nor dnf found. Exiting."
     exit 1
 fi
-
-# Install packages
 if [[ "$PKG_MANAGER" == "apt" ]]; then
     echo "Updating packages..."
     apt update
@@ -26,14 +23,12 @@ if [[ "$PKG_MANAGER" == "apt" ]]; then
     apt install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virtinst
 elif [[ "$PKG_MANAGER" == "dnf" ]]; then
     echo "Installing QEMU/KVM, libvirt, and tools..."
-    dnf install -y @virtualization qemu-kvm libvirt libvirt-daemon bridge-utils virt-install
+    dnf install -y qemu-kvm libvirt libvirt-daemon virt-install
 fi
-
 CURRENT_USER=${SUDO_USER:-$USER}
 echo "Adding $CURRENT_USER to libvirt and kvm groups..."
 usermod -aG libvirt "$CURRENT_USER"
 usermod -aG kvm "$CURRENT_USER"
-
 USER_HOME=$(eval echo "~$CURRENT_USER")
 CONFIG_DIR="$USER_HOME/.config/libvirt"
 CONFIG_FILE="$CONFIG_DIR/libvirt.conf"
